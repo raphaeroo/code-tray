cbbefcc975aa7c755fd6e49aba7bc38b89e532ec
Raphael Freitas <raphaelfreitas@pop-os.localdomain>
2020-06-12 18:32:25 -0300

    create lint config and create store to persist data

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-const { resolve } = require('path')
+const { resolve, basename } = require('path')
 const { app, Tray, Menu, dialog } = require('electron')
+
+const Store = require('electron-store')
+
+const schema = {
+  projects: {
+    type: 'string'
+  }
+}
+
+const store = new Store({ schema })
 
 process.platform === 'darwin' && app.dock.hide()
 
 app.on('ready', () => {
+  const storedProjects = store.get('projects')
+  const projects = storedProjects
+    ? JSON.parse(store.get('projects'))
+    : []
+
+  // store.clear()
+
+  console.log(projects)
+
   const tray = process.platform === 'darwin'
     ? new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'))
     : process.platform === 'linux'
@@ -17,10 +36,16 @@ app.on('ready', () => {
       checked: true,
       click: () => {
         dialog.showOpenDialog({ properties: ['openDirectory'] })
-          .then((path) => console.log(path.filePaths))
-      },
+          .then((path) => {
+            const projectPath = path.filePaths[0]
+            store.set('projects', JSON.stringify([...projects, {
+              path: projectPath,
+              name: basename(projectPath)
+            }]))
+          }).catch((err) => console.log(err)).finally(() => console.log('Projeto Adicionado'))
+      }
     }
   ])
-  
+
   tray.setContextMenu(contextMenu)
 })
